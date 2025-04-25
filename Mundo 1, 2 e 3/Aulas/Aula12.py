c2b83367c767cb21e5495efa0e08fe82ce7f9d94
VtinRD <169111643+vrenzd@users.noreply.github.com>
2025-04-25 18:16:15 -0300

    Update Aula12.py

--- Mundo 1, 2 e 3/Aulas/Aula12.py
+++ Mundo 1, 2 e 3/Aulas/Aula12.py
@@ -1,13 +1,16 @@
 from time import sleep
+
 nome = str(input('Informe seu nome: '))
-print('-=-' *10)
+print('-=-' * 10)
 print('Analisando seu nome...')
-print('-=-' *10)
+print('-=-' * 10)
 sleep(1.3)
+
 if nome == 'Victor':
-    print('Você é o rei del west')
-elif nome == 'Maria' or 'Ana' or 'Julia':
-    print('Vocês são lindas')
+    print('Você é o rei do oeste!')
+elif nome in ['Maria', 'Ana', 'Julia']:
+    print('Vocês são incríveis!')
+elif nome == 'Eduardo':
+    print('Você tem um nome forte e marcante!')
 else:
-    nome == 'Eduardo'
-    print('GAY AO EXTREMO!')
+    print(f'{nome}, você é único e especial!')
